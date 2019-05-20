1a644528e85b6cc58c0e2094e285ef65a9d93726
Nax <nzmcnarz@gmail.com>
2019-05-20 21:55:27 +0300

    Modeller, Models isimli dizene taşında daha düzenli olmak için ve bazı gerekli düzenlemeler yapıldı

--- app/Http/Controllers/PollController.php
+++ app/Http/Controllers/PollController.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class PollController extends Controller
 {
+    public function store(Request $request)
+    {
+        $poll = Poll::create(['name' => $request->get('poll_name')]);
+        $question = Question::create(['content' => $request->get('question')]);
 
+        $choices = [$request->get('a'), $request->get('b')];
+        foreach($choices as $choice) {
+            $c = Choice::create(['content' => $choice]);
+        }
+
+        return redirect('/poll')->with('success', 'Contact saved!');
+    }
 }
--- app/Models/Poll.php
+++ app/Models/Poll.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Choice extends Model
+class Poll extends Model
 {
     //
 }
