5f33a799de88c543a0f30811cd5ff7437a8e01fb
Nax <nzmcnarz@gmail.com>
2019-05-22 08:46:16 +0300

    Kullanıcının oylarını kaydetmek için store fonksiyonu yazıldı.

--- app/Http/Controllers/ChoiceController.php
+++ app/Http/Controllers/ChoiceController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Models\Choice;
+
 use Illuminate\Http\Request;
 
 class ChoiceController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $userChoices = request('userChoices');
+        $userID = request('userID');
+        $user = User::find($userID);
+        $user->choices()->attach($userChoices);
+    }
 }
