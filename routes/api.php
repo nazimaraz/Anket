99b2688f57ee9881e0ef5f4d192923d08dd67dc4
Nax <nzmcnarz@gmail.com>
2019-05-23 16:38:52 +0300

    Kullanıcının bilgilerini almak için gerekli get fonksiyonu yazıldı.

--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,7 @@ Route::group(['middleware' => 'auth:api'], function () {
     Route::patch('settings/profile', 'Settings\ProfileController@update');
     Route::patch('settings/password', 'Settings\PasswordController@update');
 
+    Route::get('/user/{userID}', 'UserController@show');
     Route::post('/poll/store', 'PollController@store');
 });
 
