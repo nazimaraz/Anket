73bdaa749489ccc56037f912379f650075ebc9fa
Nax <nzmcnarz@gmail.com>
2019-05-23 15:52:26 +0300

    Loading eklendi ve tasarımsal değişiklikler yapıldı.

--- resources/js/pages/poll/index.vue
+++ resources/js/pages/poll/index.vue
@@ -1,32 +1,44 @@
 <template>
-  <div class="row">
-    <div class="col-lg-12">
-      <router-link
-        v-for="poll in polls"
-        :key="poll.id"
-        :to="{ name: 'poll.show', params: { pollID: poll.id } }"
-      >
-        {{ poll.name }}
-        <br>
-      </router-link>
+  <section>
+    <loader v-if="isLoading"/>
+    <div v-else class="row">
+      <div class="col-lg-12">
+        <h1 v-if="polls.length === 0">{{ $t('no_poll') }}</h1>
+        <ul v-else class="list-group">
+          <router-link
+            v-for="poll in polls"
+            :key="poll.id"
+            class="list-group-item list-group-item-action"
+            :to="{ name: 'poll.show', params: { pollID: poll.id } }"
+          >{{ poll.name }}</router-link>
+        </ul>
+      </div>
     </div>
-  </div>
+  </section>
 </template>
 
 <script>
 import axios from "axios";
+import Loader from "../../components/Loader";
 
 export default {
+  components: { Loader },
   metaInfo() {
     return { title: this.$t("polls") };
   },
   data: () => ({
-    polls: []
+    polls: [],
+    isLoading: true
   }),
   created() {
-    axios.get("/api/poll/index").then(response => {
-      this.polls = response.data;
-    });
+    axios
+      .get("/api/poll/index")
+      .then(response => {
+        this.polls = response.data;
+      })
+      .then(response => {
+        this.isLoading = false;
+      });
   }
 };
 </script>
